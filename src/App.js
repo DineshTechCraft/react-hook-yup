24d4b22d870a280460e8910895fb4d6ed8f5499d
DineshTechCraft <dinesh.d2207.m@gmail.com>
2024-03-10 21:28:48 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import Formyup from "./components/form";
 
 function App() {
   return (
-    <div className="App bg-success">
-      <div className="container">
+    <div className="App bg-success container-fluid">
+      
         <div className="card">
           <Formyup />
         </div>
       </div>
-    </div>
   );
 }
 
